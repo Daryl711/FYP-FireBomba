cd1dba06cf4ef222809d99d3e61396a7349369f9
thelazycat-0728 <jonathanyeo0728@gmail.com>
2026-04-26 23:44:40 +0800

    feat: room details able to read from database

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth.routes');
+const roomDetailRoutes = require('./routes/roomDetails.routes')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', authRoutes);
+app.use('/room-detail', roomDetailRoutes);
 
 module.exports = app;
