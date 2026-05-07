19051fc99bd8774cd751738d97ab943074796c16
Henry Zheng Han SIM <104388784@students.swinburne.edu.my>
2026-05-08 00:17:22 +0800

    update json format of sensor readings and alerts

--- arduino_script/mosquitto_mqtt_communication/mosquitto_mqtt_communication.ino
+++ arduino_script/mosquitto_mqtt_communication/mosquitto_mqtt_communication.ino
@@ -145,7 +145,7 @@ void loop() {
 
       // Build JSON store sensor readings
       sensorPayload = "{";
-      sensorPayload += "\"room_id\":\"1\",";
+      sensorPayload += "\"roomId\":\"1\",";
       sensorPayload += "\"flame\":" + flame + ",";
       sensorPayload += "\"temperature\":" + temp + ",";
       sensorPayload += "\"humidity\":" + humid + ",";
@@ -187,8 +187,8 @@ void loop() {
       if (alertList != "")
       {
         alertPayload = "{";
-        alertPayload += "\"room_id\":1,";
-        alertPayload += "\"warning_titles\":[" + alertList + "]";
+        alertPayload += "\"roomId\":1,";
+        alertPayload += "\"warningTitles\":[" + alertList + "]";
         alertPayload += "}";
       } else {
         alertPayload = "";
